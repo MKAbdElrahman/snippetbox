5774961594746eb3c22abc16bc8115e806a491c8
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-21 09:25:04 +0200

    added a helloweb app

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 func main() {
-	fmt.Println("Hello World!")
+
+	host := "localhost"
+	port := 3000
+
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	log.Println("starting server on addr:", addr)
+	err := http.ListenAndServe(addr, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
