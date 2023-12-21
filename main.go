ffe3c21f213bf6d883adbec0252ce5307925d970
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-21 09:35:10 +0200

    registered routes for viewing and creating snippets

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is a snippet."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is a form to create a new snippet."))
+}
+
 func main() {
 
 	host := "localhost"
@@ -19,6 +28,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("starting server on addr:", addr)
 	err := http.ListenAndServe(addr, mux)
