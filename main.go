5cf42ce9005870f1fedba82eab8048884e84df1d
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-21 10:18:03 +0200

    refactor: use http.Error shortcut for  error responses

--- main.go
+++ main.go
@@ -22,8 +22,9 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("method not allowed"))
+
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+
 		return
 	}
 	w.Write([]byte("snippet created..."))
