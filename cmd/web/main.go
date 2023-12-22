682018cb1d822b6c8803e28b2b206f9f1887abee
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-22 15:07:42 +0200

    externally config through cmd options and env variables

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ardanlabs/conf/v3"
 )
 
 func main() {
 
-	host := "localhost"
-	port := 3000
+	cfg := struct {
+		conf.Version
+		Web struct {
+			Host string `conf:"default:0.0.0.0:3000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: "v1.0.0",
+			Desc:  "Snippetbox",
+		},
+	}
 
-	addr := fmt.Sprintf("%s:%d", host, port)
+	const prefix = "Snippetbox"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return
+		}
+		log.Fatalf("parsing config: %v", err)
+	}
 
 	mux := http.NewServeMux()
 
@@ -22,8 +42,8 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("starting server on addr:", addr)
-	err := http.ListenAndServe(addr, mux)
+	log.Println("starting server on addr:", cfg.Web.Host)
+	err = http.ListenAndServe(cfg.Web.Host, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module snippetbox
 
 require github.com/a-h/templ v0.2.476
 
+require github.com/ardanlabs/conf/v3 v3.1.7
+
 go 1.21.4
