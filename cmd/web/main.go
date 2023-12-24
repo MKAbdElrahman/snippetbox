45b9af042a09712e4bbd165c7f06431be06ed00a
MKAbdElrahman <mkcodedev@gmail.com>
2023-12-24 13:53:34 +0200

    added database migrations

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"net/http"
@@ -10,6 +11,8 @@ import (
 	"snippetbox/snippet"
 
 	"github.com/ardanlabs/conf/v3"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -18,6 +21,10 @@ func main() {
 		conf.Version
 		Web struct {
 			Host string `conf:"default:0.0.0.0:3000"`
+		}
+
+		Mysql struct {
+			Dsn string `conf:"default:user:password@/snippetbox?parseTime=true"`
 		}
 	}{
 		Version: conf.Version{
@@ -46,6 +53,19 @@ func main() {
 		logger.WithLineSource(true, logger.ErrorLevel, logger.InfoLevel),
 	)
 
+	fmt.Println(cfg.Mysql.Dsn)
+	db, err := openDB(cfg.Mysql.Dsn)
+	if err != nil {
+		logger.Error("failed to open db", map[string]any{
+			"error": err.Error(),
+		})
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	logger.Info("connected to mysql", map[string]any{})
+
 	mux := http.NewServeMux()
 	RegisterRoutes(mux, logger)
 
@@ -72,4 +92,15 @@ func RegisterRoutes(mux *http.ServeMux, logger *logger.Logger) {
 	mux.HandleFunc("/", homeHander.HandleRenderFullPage)
 	mux.HandleFunc("/snippet/view", snippetHandler.HandleView)
 	mux.HandleFunc("/snippet/create", snippetHandler.HandleCreate)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
---
+++ mysql/migrations/000001_create-snippets-table.down.sql
@@ -0,0 +1,5 @@
+-- Drop the index on the created column.
+DROP INDEX idx_snippets_created ON snippets;
+
+-- Drop the `snippets` table.
+DROP TABLE snippets;
