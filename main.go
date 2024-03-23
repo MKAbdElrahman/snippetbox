903165da9957ba030027bdb7951131e40e269342
mkabdelrahman <mkcodedev@gmail.com>
2024-03-23 19:38:29 +0200

    registered a home page handler

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/charmbracelet/log"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	const port = 3000
+	host := "localhost"
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", home)
+
+	log.Info("starting server", "host", host, "port", port)
+	err := http.ListenAndServe(addr, mux)
+	log.Fatal("server error", err)
 }
